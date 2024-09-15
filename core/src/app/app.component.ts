78685705f6f0dabd5e8b4f17f86ca8fd2ceb82d0
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-09-15 20:19:42 +0000

    review code

--- core/src/app/app.component.ts
+++ core/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CommonModule } from '@angular/common';
-
+import { AboutComponent } from './about/about.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CommonModule],
+  imports: [RouterOutlet, HomeComponent, AboutComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- core/src/app/app.routes.ts
+++ core/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-
+import { AppComponent } from './app.component';
 
 
 export const routes: Routes = [
 
     {path: "", redirectTo: "", pathMatch: 'full'},
-    {path: "" , component : HomeComponent},
+    {path: "" , component : AppComponent},
     
 
 ];
